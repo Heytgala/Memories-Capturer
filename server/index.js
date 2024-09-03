e684fab75c0d1543d830893c79641522125bd4a2
Heytgala <heytgala26@gmail.com>
2024-09-03 00:03:39 -0500

    updated few bugs

--- server/index.js
+++ server/index.js
@@ -3,9 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose, { mongo } from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
 
 const app = express();
-
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit: "100mb", extended: true }));
@@ -13,10 +14,9 @@ app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-const CONNECTION_URL = 'mongodb+srv://heytgala26:Heyt%40123@cluster0.e4n23.mongodb.net/';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
